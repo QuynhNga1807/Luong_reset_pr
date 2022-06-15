b3f0918264259c8deecc61121185b5a5c021908f
QuynhNga1807 <107568037+QuynhNga1807@users.noreply.github.com>
2022-06-15 22:35:07 +0700

    Update orders.php

--- admin/orders/orders.php
+++ admin/orders/orders.php
@@ -1,6 +1,3 @@
-<?php
-require_once ('../admin/connection/db_conn.php');
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,7 +23,16 @@ require_once ('../admin/connection/db_conn.php');
 			<div class="panel-body">
 				<form method="post" enctype="multipart/form-data">
 					<div class="form-group" style="margin-top:30px">
-					<h2 style="text-align:center">Đơn hàng</h2>
+					<table  class="table table-bordered table-hover">
+                        <tr>
+                            <th>Mã Đơn hàng</th>
+                            <th>Khách hàng</th>
+                            <th>Ngày đặt</th>
+                            <th>Nơi giao</th>
+                            <th>Trạng thái </th>
+						
+                        </tr>
+                    <tbody>
 					  <?php 
 					  $sql= 'SELECT products.name, products.Thumbnail, order_details.price, order_details.num, order_details.order_id, orders.order_id, orders.order_date,
 					  				orders.address, orders.status, orders.fullname,orders.phone_number
@@ -35,55 +41,35 @@ require_once ('../admin/connection/db_conn.php');
 								JOIN orders ON orders.order_id = order_details.order_id
 								GROUP BY products.name, products.Thumbnail, order_details.price, order_details.num, order_details.order_id, orders.order_id, orders.order_date,
 					  				orders.address, orders.status, orders.fullname,orders.phone_number';
-					  $result = mysqli_query($conn, $sql);
-					  $data = [];
-					  while ($row = mysqli_fetch_array($result)) {
-						  $data[] = array(
-							  'order_id' => $row['order_id'],
-							  'order_date' => date('d/m/Y H:i:s', strtotime($row['order_date'])),
-							  'address' => $row['address'],
-							  'status' => $row['status'],
-							  'fullname' => $row['fullname'],
-							  'name' => $row['name'],
-							  'phone_number' => $row['phone_number'],
-						  );
-					  }
+						$result = executeResult($sql);
+						foreach ($result as $item) {
 					  ?>
-						<table  class="table table-bordered table-hover">
-                        <tr>
-                            <th>Mã Đơn hàng</th>
-                            <th>Khách hàng</th>
-                            <th>Ngày đặt</th>
-                            <th>Nơi giao</th>
-                            <th>Trạng thái thanh toán</th>
-                        </tr>
-                    <tbody>
-					<?php foreach ($data as $dondathang)
-						?>
 						
                             <tr>
-                                <td><?= $dondathang['order_id'] ?></td>
-                                <td><b><?= $dondathang['fullname'] ?></b><br />(<?= $dondathang['phone_number'] ?>)</td>
-                                <td><?= $dondathang['order_date'] ?></td>
-                                <td><?= $dondathang['address'] ?></td>
+                                <td><?= $item['order_id'] ?></td>
+                                <td><b><?= $item['fullname'] ?></b><br />(<?= $item['phone_number'] ?>)</td>
+                                <td><?= $item['order_date'] ?></td>
+                                <td><?= $item['address'] ?></td>
                                 <td>
-                                    <?php if ($dondathang['status'] == 0) : ?>
+                                    <?php if ($item['status'] == 0) : ?>
                                         <span class="badge badge-danger">Chưa xử lý</span>
-                                    <?php else : ?>
-                                        <span class="badge badge-success">Đã giao hàng</span>
-                                    <?php endif;?>
-                                
-									 <!-- Đơn hàng nào chưa thanh toán thì được phép phép Xóa, Sửa -->
-									 <?php if ($dondathang['status'] == 0) : ?>
-                                        <!-- Nút sửa, bấm vào sẽ hiển thị form hiệu chỉnh thông tin dựa vào khóa chính `dh_ma` -->
-										&nbsp;&nbsp; <a href="index.php?page_layout=editorders&id=<?= $dondathang['order_id'] ?>" class="btn btn-warning">
+										&nbsp;&nbsp; <a href="index.php?page_layout=editorders&id=<?= $item['order_id'] ?>" class="btn btn-warning">
                                          Sửa
                                         </a>
                                     <?php else : ?>
-
-                                    <?php endif; ?>
+                                    <?php endif;?>
+									<?php if ($item['status'] == 2) : ?>
+										<span class="badge badge-success">Đã giao hàng</span>
+										<?php else : ?>
+                                    <?php endif;?>
+									<?php if ($item['status'] == 1) : ?>
+										<span class="badge badge-success">Đang giao hàng</span>
+                                    <?php else : ?>
+                                    <?php endif;?>
                                 </td>
+								
                             </tr>
+							<?php }?>
                     </tbody>
                 </table>
 							    <!-- End block content -->
@@ -91,6 +77,7 @@ require_once ('../admin/connection/db_conn.php');
         </div>
     </div>		
 	</div>
-	
+
 </body>
 </html>
+
